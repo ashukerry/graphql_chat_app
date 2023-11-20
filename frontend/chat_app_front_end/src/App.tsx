47310a8ee2ab89dfd05405c9fb0284c9b5b0fe5c
ashukerry <yourgitemail@example.com>
2023-11-20 10:45:38 +0100

    frontend

---
+++ frontend/chat_app_front_end/src/App.tsx
@@ -0,0 +1,17 @@
+import { Card, Text } from "@mantine/core";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <p className="read-the-docs">
+        Click on the Vite and React logos to learn more 
+      </p> */}
+      <Card>
+        <Text>Hello</Text>
+      </Card>
+    </>
+  );
+}
+
+export default App;
